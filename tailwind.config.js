d46ea645795303bb18dfa03ae980b0b72e1f1ac9
rollixapo <e17.foldi.noel@palfy-vizugyi.hu>
2023-01-14 23:42:25 +0100

    téleg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
   theme: {
     extend: {},
     fontFamily: {
