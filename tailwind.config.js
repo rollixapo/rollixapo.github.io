d72aa1f6e8cb857d077d639542ff35fd9a8b798c
rollixapo <e17.foldi.noel@palfy-vizugyi.hu>
2023-01-14 17:57:59 +0100

    Kész minden helo
    Első feltöltése a portfoliomnak

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'oswald': ['"Oswald"', 'sans-serif'],
+      'slab': ['"Roboto Slab"', 'serif'],
+    },
+  },
+  plugins: [],
+}
